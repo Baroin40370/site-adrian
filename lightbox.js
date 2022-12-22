443bc2e3e7a6dc00bb7f3ef5b8c63cf213df6f7c
Baroin40370 <torai@live.fr>
2022-12-22 14:43:35 +0100

    sauvegarde

--- lightbox.js
+++ lightbox.js
@@ -2,7 +2,6 @@ window.onload = () => {
     const lightbox = document.querySelector('.lightbox');
     const close = document.querySelector('.close');
     const portafolio = document.querySelector('#portafolio');
-    portafolio.addEventListener('click',onCartClick,true);
     function onCartClick(event) {
     const {target} = event;
     const {src} = target; 
@@ -11,6 +10,7 @@ window.onload = () => {
     console.log(target.src);
     lightbox.classList.add('active');
     }
+    portafolio.addEventListener('click',onCartClick,true);
     
     close.addEventListener("click",function(){
         lightbox.classList.remove('active');
